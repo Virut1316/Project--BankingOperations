a9399cd58352cf2a92cdad593990ebff6a02287b
Virut1316 <54875369+Virut1316@users.noreply.github.com>
2021-10-12 00:34:02 -0400

    SetUp for testing files

---
+++ Project0-BankingOperations-master/src/test/java/com/revature/test/LoginService/Placeholder.java
@@ -0,0 +1,11 @@
+package com.revature.test.LoginService;
+
+
+public interface Placeholder {
+	
+	/*
+	 * This is a placeholder file created simple to maintain the Maven file structure on GitHub. 
+	 * You may immediately delete this file from your project, you will not require it. 
+	 */
+
+}
